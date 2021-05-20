ffa8913c2254ab9d718938f402b3779bcc54613b
aeroej <asd0755@naver.com>
2021-05-21 00:23:08 +0900

    Write section 6-6 공주구하기

--- inflearn/6_자료구조/6_공주구하기.js
+++ inflearn/6_자료구조/6_공주구하기.js
@@ -1,4 +1,17 @@
 function solution(n, k){
+    let cnt = 0;
+    let queue = [];
+    for (let i=1; i<=n; i++) queue.push(i);
+
+    while (queue.length !== 1) {
+        cnt++;
+        if (cnt === k) {
+            queue.shift();
+            cnt = 0;
+        }
+        else queue.push(queue.shift());
+    }    
+    return queue.shift();
 }
 
 console.log(solution(8, 3));
