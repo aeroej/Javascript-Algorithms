81a661e65d47d60911ffd3a66f609fa2ef8b1963
aeroej <asd0755@naver.com>
2021-05-21 09:57:06 +0900

    Write section 6-7 교육과정설계

---
+++ inflearn/6_자료구조/7_교육과정설계.js
@@ -0,0 +1,15 @@
+function solution(need, plan){
+    let queue = [];
+    for (let x of need) {
+        queue.push(x);
+    }
+    for (let x of plan) {
+        if (x === queue[0]) queue.shift()
+    }
+    if (queue.length) return "NO";
+    else return "YES"
+}
+
+let a="CBA";
+let b="CBDAGE";
+console.log(solution(a, b));
