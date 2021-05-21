03c7d6f4524b21d4409b8493316edae4c44cf0ff
aeroej <asd0755@naver.com>
2021-05-21 14:24:53 +0900

    Write section 7-3 SpecialSort

--- inflearn/7_정렬_그리디_결정알고리즘/3_SpecialSort.js
+++ inflearn/7_정렬_그리디_결정알고리즘/3_SpecialSort.js
@@ -1,1 +1,18 @@
-{\rtf1}
+// 버블정렬
+// i는 arr.length-1부터 시작해서 i>1 i++
+// j는 0부터 시작하여 j<i, j++
+function solution(arr){
+    for (let i=arr.length-1; i>1; i--) {
+        for (let j=0; j<i; j++) {
+            if (arr[j]>0 && arr[j+1]<0) {
+                let tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+    return arr;
+}
+
+let arr=[1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(arr));
