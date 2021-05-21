f350ba83e0b5321b0d33a81a8a3fb21d79bae041
aeroej <asd0755@naver.com>
2021-05-21 15:29:03 +0900

    Write section 7-5 LRU

---
+++ inflearn/7_정렬_그리디_결정알고리즘/5_LRU.js
@@ -0,0 +1,28 @@
+// queue같다. 새로운 작업이 생겼는데 캐시에 없다면, queue.pop()하고 queue.push(새로운작업)
+// 새로운 작업이 기존 캐시에 있다면, queue.includes(새로운작업) --> queue.splice(캐시, 1) 하고 queue.push(작업)
+// 
+
+function solution(size, arr){
+    let queue = [];
+    for (let i=0; i<arr.length; i++) {
+        if (!queue.includes(arr[i])) { // 큐에 없으면
+            if (queue.length<5) queue.push(arr[i]);
+            else {
+                queue.shift();
+                queue.push(arr[i]);
+            }
+        }
+        else { // 큐에 있으면. 캐시가 있으면
+            queue.splice(queue.indexOf(arr[i]), 1);
+            queue.push(arr[i]);
+        }
+    }
+    return queue.reverse().join(' ');
+}
+
+let arr=[1, 2, 3, 2, 6, 2, 3, 5, 7];
+console.log(solution(5, arr));
+
+// [1, 6, 2, 3, 5]
+// [6, 2, 3, 5, 7]
+// 역순으로 출력하면 될듯 queue.reverse()
