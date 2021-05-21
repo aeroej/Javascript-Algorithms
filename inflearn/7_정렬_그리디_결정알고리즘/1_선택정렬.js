71ed3dbaf6298af441ffe35f2e0fc1a5339b05c4
aeroej <asd0755@naver.com>
2021-05-21 11:56:06 +0900

    Modify section 7-1 선택정렬

--- inflearn/7_정렬_그리디_결정알고리즘/1_선택정렬.js
+++ inflearn/7_정렬_그리디_결정알고리즘/1_선택정렬.js
@@ -1,19 +1,22 @@
-// arr를 훑어보고 가장 작은 값Math.min을 뽑아서 result arr에 추가
+// 선택정렬 : 최소값을 찾는다, 맨 앞에 위치한 값과 교체한다
+// 새로운 array를 정의하지 않는다. 
+// 교체 --> 인덱스 교체만 하면 될듯
+// i와 j,,, 교환에 필요한 변수 tmp
+// i를 두고, j=i+1에서부터 시작, arr[i]보다 작은 arr[j]값을 발견하면 교환
 
 // *** arr.splice(index, 몇개제거할건지) ***
-function solution(arr){
-    let result = Array.from({length:arr.length}, (v, i) => Number.MAX_SAFE_INTEGER);
-    for (let i=0; i<result.length; i++) {
-        let index = -1;
-        for (let j=0; j<arr.length; j++) {
-            if (arr[j] < result[i]) {
-                result[i] = arr[j];
-                index = j;
+// 변수 동시할당 [x, y] = ['a', 'b']
+function solution(arr) {
+    for (let i=0; i<arr.length; i++) {
+        for (let j=i+1; j<arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                tmp = arr[i];        
+                arr[i] = arr[j];
+                arr[j] = tmp;
             }
-        }  
-        arr.splice(index, 1);
+        }
     }
-    return result;
+    return arr;
 }
 
 let arr=[13, 5, 11, 7, 23, 15];
