057f19b4b7e9506d3fbbc7a84684bbd7ef7b60e7
aeroej <asd0755@naver.com>
2021-05-21 22:14:29 +0900

    Modify section 7-5 LRU

--- inflearn/7_정렬_그리디_결정알고리즘/5_LRU.js
+++ inflearn/7_정렬_그리디_결정알고리즘/5_LRU.js
@@ -2,25 +2,25 @@
 // 새로운 작업이 기존 캐시에 있다면, queue.includes(새로운작업) --> queue.splice(캐시, 1) 하고 queue.push(작업)
 // 
 
-function solution(size, arr){
+function solution(size, arr) {
     let queue = [];
-    for (let i=0; i<arr.length; i++) {
-        if (!queue.includes(arr[i])) { // 큐에 없으면
-            if (queue.length<5) queue.push(arr[i]);
-            else {
+    for (let i = 0; i < arr.length; i++) {
+        if (queue.includes(arr[i])) { // 캐시에 있는 경우
+            queue.splice(queue.indexOf(arr[i]), 1);
+            queue.push(arr[i]);
+        }
+        else { // 캐시에 없는 경우
+            if (queue.length < size) queue.push(arr[i]);
+            else { 
                 queue.shift();
                 queue.push(arr[i]);
             }
-        }
-        else { // 큐에 있으면. 캐시가 있으면
-            queue.splice(queue.indexOf(arr[i]), 1);
-            queue.push(arr[i]);
         }
     }
     return queue.reverse().join(' ');
 }
 
-let arr=[1, 2, 3, 2, 6, 2, 3, 5, 7];
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
 console.log(solution(5, arr));
 
 // [1, 6, 2, 3, 5]
