6484326ce19cca098bf5092fa22a8a8a9cc424e6
aeroej <asd0755@naver.com>
2021-05-23 02:25:17 +0900

    Modify section 7-10 이분탐색

--- inflearn/7_정렬_그리디_결정알고리즘/10_이분탐색.js
+++ inflearn/7_정렬_그리디_결정알고리즘/10_이분탐색.js
@@ -1,14 +1,24 @@
-function solution(target, arr){
+// function solution(target, arr){
+//     arr.sort((a, b) => a-b);
+//     let key = Math.floor(arr.length/2);
+//     for (let i=key/2; i>=0; i=Math.floor(i/2)) {
+//         if (arr[key] === target) return key+1;
+//         else if (arr[key] > target) key -= i
+//         else key += i
+//         console.log(i, key, arr[key])
+//     }
+// }
+
+function solution(target, arr) {
     arr.sort((a, b) => a-b);
-    let key = Math.floor(arr.length/2);
-    for (let i=key/2; i>=0; i=Math.floor(i/2)) {
-        if (arr[key] === target) return key+1;
-        else if (arr[key] > target) key -= i
-        else key += i
-        console.log(i, key, arr[key])
+    let lt = 0, rt = arr.length-1;
+    while(true) {
+        let mid = Math.floor((lt+rt)/2);
+        if (arr[mid] === target) return mid+1;
+        else if (arr[mid] > target) rt = mid;
+        else lt = mid;
     }
 }
 
-// 12, 19, 23, 32, 57, 65, 81, 87, 99
-let arr=[23, 87, 65, 12, 57, 32, 99, 81];
+let arr=[23, 87, 65, 12, 57, 32, 99, 81, 19, 99];
 console.log(solution(32, arr));
