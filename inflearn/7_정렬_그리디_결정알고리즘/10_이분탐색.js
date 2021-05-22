34f9bca5506909133719b42ea037108ca0b91141
aeroej <asd0755@naver.com>
2021-05-23 01:22:47 +0900

    Write section 7-10 이분검색

--- inflearn/7_정렬_그리디_결정알고리즘/10_이분탐색.js
+++ inflearn/7_정렬_그리디_결정알고리즘/10_이분탐색.js
@@ -1,12 +1,14 @@
 function solution(target, arr){
     arr.sort((a, b) => a-b);
     let key = Math.floor(arr.length/2);
-    for (let i=key/2; i>=0; i/2) {
+    for (let i=key/2; i>=0; i=Math.floor(i/2)) {
         if (arr[key] === target) return key+1;
         else if (arr[key] > target) key -= i
         else key += i
+        console.log(i, key, arr[key])
     }
 }
 
+// 12, 19, 23, 32, 57, 65, 81, 87, 99
 let arr=[23, 87, 65, 12, 57, 32, 99, 81];
 console.log(solution(32, arr));
