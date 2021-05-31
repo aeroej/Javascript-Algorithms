be86738bdbf77db0afee82f0db2c61d3bab89eaa
asd0755 <asd0755@naver.com>
2021-05-31 15:47:58 +0900

    Write programmers level1 연습문제

--- programmers/level1/핸드폰번호가리기.js
+++ programmers/level1/핸드폰번호가리기.js
@@ -1,17 +1,15 @@
 /*
-문제 설명
-프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
-전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+arr.slice() --> 두번째 매개변수를 안적으면 배열 끝까지 간다
+arr.slice(n-4) 말고 arr.slice(-4)만 해줘도 된다
 
-제한 조건
-s는 길이 4 이상, 20이하인 문자열입니다.
-입출력 예
-phone_number	return
-"01033334444"	"*******4444"
-"027778888"	"*****8888"
+더 나은 코드
+let result = '*'.repeat(s.length - 4) + s.slice(-4);
 */
-
 function solution(phone_number) {
-  var answer = '';
+  const n = phone_number.length;
+  var answer = '*'.repeat(n-4);
+  answer += phone_number.slice(n-4); 
   return answer;
 }
+
+console.log(solution('01033334444'))
