7bd70b32986fcb6bd516d977713b977c92b1dde6
asd0755 <asd0755@naver.com>
2021-06-17 17:07:14 +0900

    index.js .md 수정

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-function solution(numbers) {
-  let arr = [];
-  for (let i=0; i<numbers.length-1; i++) {
-    for (let j=i+1; j<numbers.length; j++) {
-      arr.push(numbers[i] + numbers[j])
-    }
-  }
-  return [...new Set(arr)].sort((a, b) => a-b);
+function solution() {
+
+  return "solution";
 }
 
-console.log(solution([2,1,3,4,1]))
-// result [2,3,4,5,6,7]
+console.log(solution())
