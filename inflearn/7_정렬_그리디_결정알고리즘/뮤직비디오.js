6f414158fee9fb773f0c7200a7b7367fd3148c60
aeroej <asd0755@naver.com>
2021-06-30 19:54:16 +0900

    인프런7 뮤직비디오 작성

--- inflearn/7_정렬_그리디_결정알고리즘/뮤직비디오.js
+++ inflearn/7_정렬_그리디_결정알고리즘/뮤직비디오.js
@@ -1,6 +1,28 @@
-function solution(m, arr) {
-    return 'solution';
+function count(mid, arr) {
+    let cnt = 1;
+    let sum = 0;
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        sum += arr[i];
+        if (sum > mid) {
+            cnt++;
+            sum = arr[i]
+        }
+    }
+    return cnt;
 }
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(solution(3, arr));
+function solution(m, arr) {
+    let left = arr[arr.length - 1], right = 0;
+    for (let x of arr) right += x;
+    arr.sort((a, b) => a - b);
+
+    while (left <= right) {
+        let mid = Math.floor((right + left) / 2);
+        let cnt = count(mid, arr);
+
+        if (cnt === m) return mid;
+        else if (cnt < m) right = mid - 1;  // 저장용랑이 크다
+        else if (cnt > m) left = mid + 1;  // 저장용량이 부족하다
+    }
+}
