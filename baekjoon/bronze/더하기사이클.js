924b66e4dceebf72b917e3b4fbc2fefcb2af6e05
aeroej <asd0755@naver.com>
2021-07-19 14:40:05 +0900

    브론즈 더하기 사이클 작성

---
+++ baekjoon/bronze/더하기사이클.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+let input = fs.readFileSync('../stdin').toString().split('\n');
+let N = parseInt(input[0]);
+
+function solution(N) {
+  let cnt = 0;
+  let num = N;
+  
+  while (true) {
+    let sum = parseInt(num%10 + num/10);
+    num = (num%10)*10 + sum%10;
+    cnt++;
+    if (num === N) break;
+  }
+  return cnt;
+}
+
+console.log(solution(N))
